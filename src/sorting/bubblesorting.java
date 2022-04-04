5ba6302863d560fe10e0eaf6ff78816a3bfdd016
DIVYANSHI <DIVYANSHI@10.12.12.55>
2022-04-04 11:51:10 +0530

    final oldsort

--- src/sorting/bubblesorting.java
+++ src/sorting/bubblesorting.java
@@ -1,37 +1,39 @@
-// Java program for implementation of Bubble Sort
-class BubbleSort
-{
-	void bubbleSort(int arr[])
-	{
-		int n = arr.length;
-		for (int i = 0; i < n-1; i++)
-			for (int j = 0; j < n-i-1; j++)
-				if (arr[j] > arr[j+1])
-				{
-					// swap temp and arr[i]
-					int temp = arr[j];
-					arr[j] = arr[j+1];
-					arr[j+1] = temp;
-				}
-	}
+// Bubble sort in Java
 
-	/* Prints the array */
-	void printArray(int arr[])
-	{
-		int n = arr.length;
-		for (int i=0; i<n; ++i)
-			System.out.print(arr[i] + " ");
-		System.out.println();
-	}
+import java.util.Arrays;
 
-	// Driver method to test above
-	public static void main(String args[])
-	{
-		BubbleSort ob = new BubbleSort();
-		int arr[] = {64, 34, 25, 12, 22, 11, 90};
-		ob.bubbleSort(arr);
-		System.out.println("Sorted array");
-		ob.printArray(arr);
-	}
+class Main {
+
+  // perform the bubble sort
+  static void bubbleSort(int array[]) {
+    int size = array.length;
+    
+    // loop to access each array element
+    for (int i = 0; i < size - 1; i++)
+    
+      // loop to compare array elements
+      for (int j = 0; j < size - i - 1; j++)
+
+        // compare two adjacent elements
+        // change > to < to sort in descending order
+        if (array[j] > array[j + 1]) {
+
+          // swapping occurs if elements
+          // are not in the intended order
+          int temp = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] = temp;
+        }
+  }
+
+  public static void main(String args[]) {
+      
+    int[] data = { -2, 5, 0, 11, -9 };
+    
+    // call method using class name
+    Main.bubbleSort(data);
+    
+    System.out.println("Sorted Array in Ascending Order:");
+    System.out.println(Arrays.toString(data));
+  }
 }
-/* This code is contributed by DK */
